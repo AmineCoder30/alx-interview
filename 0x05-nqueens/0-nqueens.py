103e229541d858292a93cfee0ff4e965e8316223
AmineCoder30 <99543252+AmineCoder30@users.noreply.github.com>
2024-02-08 13:07:06 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,125 +1,113 @@
 #!/usr/bin/python3
-""" A program that solves the N queens problem
+"""N queens solution finder module.
 """
-from sys import argv
+import sys
 
 
-def check_row(board, index, board_len):
-    """ Check if there is a queen in the row """
-    for r in range(board_len):
-        if board[index][r]:
-            return (False)
-
-    return (True)
-
-
-def check_r_angle(board, row, col, board_len):
-    """ Check if there is a queen in the left angle """
-    c = col
-    for r in range(row, -1, -1):
-        if c >= board_len:
-            break
-        if board[r][c]:
-            return (False)
-        c += 1
-
-    c = col
-    for r in range(row, board_len):
-        if c < 0:
-            break
-        if board[r][c]:
-            return (False)
-        c -= 1
-
-    return (True)
+solutions = []
+"""The list of possible solutions to the N queens problem.
+"""
+n = 0
+"""The size of the chessboard.
+"""
+pos = None
+"""The list of possible positions on the chessboard.
+"""
 
 
-def check_l_angle(board, row, col, board_len):
-    """ Check if there is a queen in the right angle """
-    c = col
-    for r in range(row, -1, -1):
-        if c < 0:
-            break
-        if board[r][c]:
-            return (False)
-        c -= 1
+def get_input():
+    """Retrieves and validates this program's argument.
 
-    c = col
-    for r in range(row, board_len):
-        if c >= board_len:
-            break
-        if board[r][c]:
-            return (False)
-        c += 1
-
-    return (True)
-
-
-def chek_all(board, r, c, n):
-    if not check_row(board, r, n):
-        return (False)
-
-    if not check_l_angle(board, r, c, n):
-        return (False)
-
-    return (check_r_angle(board, r, c, n))
-
-
-def main():
-    """ The Main Function """
-
-    argc = len(argv)
-    if argc != 2:
+    Returns:
+        int: The size of the chessboard.
+    """
+    global n
+    n = 0
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-
+        sys.exit(1)
     try:
-        n = int(argv[1])
+        n = int(sys.argv[1])
     except Exception:
         print("N must be a number")
-        exit(1)
-
+        sys.exit(1)
     if n < 4:
         print("N must be at least 4")
-        exit(1)
-
-    n_range = range(n)
-    i = 0
-    c = 0
-    r = i
-    board = [[0 for _ in n_range] for _ in n_range]
-    result = []
-    while i < n:
-        while (c < n):
-            found = 0
-
-            while (r < n):
-                if chek_all(board, r, c, n):
-                    board[r][c] = 1
-                    result.append([c, r])
-                    found = 1
-                    r = 0
-                    break
-                r += 1
-
-            if not found and len(result):
-                last_i = result.pop()
-                c = last_i[0]
-                r = last_i[1] + 1
-                board[last_i[1]][last_i[0]] = 0
-                continue
-            c += 1
-
-        if len(result):
-            print(result)
-            i = result[0][1]
-            last_i = result.pop()
-            c = last_i[0]
-            r = last_i[1] + 1
-            board[last_i[1]][last_i[0]] = 0
-        else:
-            return
+        sys.exit(1)
+    return n
 
 
-if __name__ == "__main__":
-    main()
+def is_attacking(pos0, pos1):
+    """Checks if the positions of two queens are in an attacking mode.
+
+    Args:
+        pos0 (list or tuple): The first queen's position.
+        pos1 (list or tuple): The second queen's position.
+
+    Returns:
+        bool: True if the queens are in an attacking position else False.
+    """
+    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
+        return True
+    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
+
+
+def group_exists(group):
+    """Checks if a group exists in the list of solutions.
+
+    Args:
+        group (list of integers): A group of possible positions.
+
+    Returns:
+        bool: True if it exists, otherwise False.
+    """
+    global solutions
+    for stn in solutions:
+        i = 0
+        for stn_pos in stn:
+            for grp_pos in group:
+                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
+                    i += 1
+        if i == n:
+            return True
+    return False
+
+
+def build_solution(row, group):
+    """Builds a solution for the n queens problem.
+
+    Args:
+        row (int): The current row in the chessboard.
+        group (list of lists of integers): The group of valid positions.
+    """
+    global solutions
+    global n
+    if row == n:
+        tmp0 = group.copy()
+        if not group_exists(tmp0):
+            solutions.append(tmp0)
+    else:
+        for col in range(n):
+            a = (row * n) + col
+            matches = zip(list([pos[a]]) * len(group), group)
+            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
+            group.append(pos[a].copy())
+            if not any(used_positions):
+                build_solution(row + 1, group)
+            group.pop(len(group) - 1)
+
+
+def get_solutions():
+    """Gets the solutions for the given chessboard size.
+    """
+    global pos, n
+    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
+    a = 0
+    group = []
+    build_solution(a, group)
+
+
+n = get_input()
+get_solutions()
+for solution in solutions:
+    print(solution)
